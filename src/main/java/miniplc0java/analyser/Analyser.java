1d87745547a1e7e2d664135fe2d0d089d0f9d2b0
Qiaoyw <63182186+Qiaoyw@users.noreply.github.com>
2020-10-25 16:44:13 +0800

    Update Analyser.java

--- src/main/java/miniplc0java/analyser/Analyser.java
+++ src/main/java/miniplc0java/analyser/Analyser.java
@@ -266,7 +266,7 @@ public final class Analyser {
 
             // 加入符号表，请填写名字和当前位置（报错用）
             String name = (String) nameToken.getValue();/* 名字 */;
-            addSymbol(name, false, false, /* 当前位置 */ nameToken.getStartPos());
+            addSymbol(name, initialized, false, /* 当前位置 */ nameToken.getStartPos());
 
             // 如果没有初始化的话在栈里推入一个初始值
             if (!initialized) {
