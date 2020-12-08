cb62c1bbdce622682eb8dfde7bf6efcf9e6796ff
Qiaoyw <63182186+Qiaoyw@users.noreply.github.com>
2020-12-08 13:10:21 +0800

    Update App.java

--- ha/src/App.java
+++ ha/src/App.java
@@ -1,10 +1,9 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.List;
 import java.util.Scanner;
 
 public class App{
-    public static void main(String[] args) throws CompileError, FileNotFoundException {
+    public static void main(String[] args) throws CompileError, IOException {
         InputStream input = new FileInputStream(args[0]);
         Scanner scanner;
         scanner = new Scanner(input);
@@ -12,5 +11,18 @@ public class App{
         Tokenizer tokenizer = new Tokenizer(iter);
         Analyser analyzer = new Analyser(tokenizer);
         analyzer.analyse();
+
+        //输出格式转换
+        Target target = new Target(analyzer.globalmap, analyzer.functionmap);
+        List<Byte>  result= target.out();
+        byte[] chu = new byte[result.size()];
+        for (int i = 0; i < result.size(); ++i) chu[i] = result.get(i);
+        
+
+        //输出
+//   DataOutputStream output = new DataOutputStream(new FileOutputStream(new File("output.txt")));
+        DataOutputStream output = new DataOutputStream(new FileOutputStream(new File(args[1])));
+        output.write(chu);
     }
+    
 }
