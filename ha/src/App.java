87f9c3265e8f01e3d7845f7b62dec59d609d8f53
Qiaoyw <63182186+Qiaoyw@users.noreply.github.com>
2020-12-10 13:10:57 +0800

    Update App.java

--- ha/src/App.java
+++ ha/src/App.java
@@ -2,30 +2,39 @@ import java.io.*;
 import java.util.List;
 import java.util.Scanner;
 
-public class App {
+public class App{
     public static void main(String[] args) throws CompileError, IOException {
-        File file = new File(args[0]);
-        try {
-            FileReader fr = new FileReader(file);
-            BufferedReader reader = new BufferedReader(fr);
-            String str = reader.readLine();
-            while (str != null) {
-                System.out.println(str);
-                str = reader.readLine();
-            }
-        } catch (FileNotFoundException e) {
-            //当抛出多个异常时，子异常当在父异常前抛出。
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        //InputStream input = new FileInputStream(args[0]);
+        InputStream input = new FileInputStream("input.txt");
+        Scanner scanner;
+        scanner = new Scanner(input);
+        StringIter iter = new StringIter(scanner);
+        Tokenizer tokenizer = new Tokenizer(iter);
+        Analyser analyzer = new Analyser(tokenizer);
+        analyzer.analyse();
+
+
+        System.out.println("全局符号表大小："+analyzer.globalmap.size());
+        System.out.println("全局符号表：");
+        for (Global global : analyzer.globalmap) {
+            System.out.println(global);
+        }
+        System.out.println("函数：");
+        for (Function function : analyzer.functionmap) {
+            System.out.println(function);
         }
 
+        //输出格式转换
+        Target target = new Target(analyzer.globalmap, analyzer.functionmap);
+        List<Byte> result= target.out();
+        byte[] chu = new byte[result.size()];
+        for (int i = 0; i < result.size(); ++i) chu[i] = result.get(i);
 
 
         //输出
-        //DataOutputStream output = new DataOutputStream(new FileOutputStream(new File("output")));
-        DataOutputStream output = new DataOutputStream(new FileOutputStream(new File(args[1])));
+         DataOutputStream output = new DataOutputStream(new FileOutputStream(new File("output")));
+        //DataOutputStream output = new DataOutputStream(new FileOutputStream(new File(args[1])));
+        output.write(chu);
     }
-
 
 }
